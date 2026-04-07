c4075cdfa3ecf588dd8c29dba0f1d13f4fa02e34
Niselle <edlnid23@htl-kaindorf.at>
2026-04-07 11:19:23 +0200

    Add all missing functions main.py

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ def main():
 
 def multiplication(a, b):
     return a * b
+
+def division(a, b):
+    return a / b
+
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
